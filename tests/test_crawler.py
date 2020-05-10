1adbfe256bc0e2a4fe90feeaf2e297fc3a325102
b2aff6009 <b2aff6009@gmail.com>
2020-05-10 13:34:33 +0200

    Improved test for service

--- tests/test_crawler.py
+++ tests/test_crawler.py
@@ -4,6 +4,7 @@ import os
 import sys
 import inspect
 import json
+import datetime 
 current_dir = os.path.dirname(os.path.abspath(inspect.getfile(inspect.currentframe())))
 parent_dir = os.path.dirname(current_dir)
 sys.path.insert(0, parent_dir)
@@ -52,24 +53,38 @@ def test_load():
     assert len(mCrawler.memo) == 1, "Crawler memo contains not exactly one item"
     assert mCrawler.memo[0] == dummyName, "Crawlers memo contains {} instead of {}".format(mCrawler.memo[0], dummyName) 
 
+cnt = 0
 def test_service():
+    global cnt
     settings = baseSettings
     settings["service"] = True
     settings["sleep"] = 1
     settings["onlyOnce"] = True
     cnt = 0
     mId = 3
+    cycles = 10
 
-    def callback(file, id):
+    def callback(file, id, processingCrawler):
+        global cnt
         cnt = cnt + 1
         assert id == mId, "Argurments doesn't match the expected. Got {} instead of {}".format(id, mId)
+        if cnt >= cycles:   
+            processingCrawler.settings["service"] = False
 
     mCrawler = crawler.createCrawler(settings, callback)
-    mCrawler.process(mId)
+    startTime = datetime.datetime.now()
+    mCrawler.process(mId, mCrawler)
+    endTime = datetime.datetime.now()
+    diffTime = endTime - startTime
+    def checkTime(seconds):
+        if seconds > (cycles-1)*settings["sleep"] and seconds < (cycles+1)*settings["sleep"]:
+            return True
+        return False
+
+    assert checkTime(diffTime.seconds), "Test took {}s, expceted time would be {}s".format(diffTime.seconds, cycles*settings["sleep"])
+    assert cnt == cycles, "Wrong number of cycles. Got {} instead of {}".format(cnt, cycles)
 
         
-
-
 if __name__ == "__main__":
     #test_createCrawlerFactory()
     test_service()
